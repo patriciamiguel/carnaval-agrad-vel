a9f6fde2879af1b5f500eb5545726dde0d1ce57b
patriciamiguel <91090710+patriciamiguel@users.noreply.github.com>
2021-10-19 19:45:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ if(snake[0].y < 0 && direction =="up") snake[0].y = 16 * box;
 for(i=1; i < snake.length; i++){
     if(snake [0].x == snake [i].x && snake [0].y == snake [i].y){
         clearInterval(iniciarJogo)
-        alert("SE FUDEU :p")
+        alert("PERDEU :p")
     }
 
 }
